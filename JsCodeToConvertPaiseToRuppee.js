bad1bc331657a1852676de65e1ac0c48a7ccafc1
WaghmareAkash <waghmareakash64@gmail.com>
2024-03-28 00:12:21 +0530

    My First commit

---
+++ JsCodeToConvertPaiseToRuppee.js
@@ -0,0 +1,13 @@
+/* arith -op.js */
+console.log("<pre>");
+let data = 3025;
+let p, r;
+p= data % 100;
+r = (data-p)/ 100;
+console.log("*** Operations mod and / ***"+ "<br>");
+console.log(" 3025 paise to rupees and paise");
+console.log("rupees = "+ r);
+console.log("paise ="+ p);
+
+
+
